b20ddefcbf07791f151049983ea063d0d0c66907
yadullah <yadullas@metropolia.fi>
2023-10-09 19:40:34 +0300

    backend user profile changes

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const User = require("../models/userModel");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const getUserById = async (req, res, next) => {
   try {
@@ -47,21 +49,38 @@ const changeDetails = async (req, res, next) => {
   try {
     const userId = req.user._id;
     if (!mongoose.Types.ObjectId.isValid(userId)) {
-      return res.status(404).json({ error: "No such user found" });
+      return res.status(404).json({ message: "No such user found" });
     }
 
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ message: "User not found" });
     }
-    const updatedUser = await User.findOneAndUpdate(user._id, req.body, {
-      new: true
-    });
+
+    let newPassword;
+
+    if (req.body.password) {
+      if (!validator.isStrongPassword(req.body.password)) {
+        return res.status(400).json({ message: "Password not strong enough" });
+      }
+      const salt = await bcrypt.genSalt(13);
+      newPassword = await bcrypt.hash(req.body.password, salt);
+    }
+
+    // Update the user's details with the updated user object using a query
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: userId },
+      { ...req.body, password: newPassword },
+      {
+        new: true,
+      }
+    );
+
     res.status(200).json({
-      message: "Details for " + updatedUser.last_name + " has been updated",
+      message: "Details for " + updatedUser.last_name + " have been updated",
     });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ message: err.message });
   }
 };
 
