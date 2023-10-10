f9e99768579a6c1e8f1ce84dd0e376401995ac45
Théo Mirabile <theo.mirabile@me.com>
2023-10-10 16:53:51 +0300

    increased request limit

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: false }));
 
 const listingRouter = require("./routers/listing");
